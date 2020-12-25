a91d5a73dc6c15b8b8e4033be1f0dd9849840d83
Miguel Armas <miguel.armas@jycleadingcorp.com>
2020-12-25 14:15:00 -0500

    master: estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PersonasComponent } from './components/personas/personas.component';
+import { LoginComponent } from './components/login/login.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    PersonasComponent,
+    LoginComponent,
+    PerfilComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
