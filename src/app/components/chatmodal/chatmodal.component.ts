fbb2f86637033a7facfb299d869cbc36fc87d2d4
Miguel Armas <miguel.armas.abt@gmail.com>
2021-01-30 12:46:42 -0500

    front-cambios: envio de mensajes no reactivo

--- src/app/components/chatmodal/chatmodal.component.ts
+++ src/app/components/chatmodal/chatmodal.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Mensaje } from '../../models/mensaje';
+import { MensajeService } from '../../services/mensaje.service';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-chatmodal',
@@ -14,14 +17,12 @@ export class ChatmodalComponent implements OnInit {
   textoNuevoMensaje: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-  public modalRef: MatDialogRef<ChatmodalComponent>) { }
+  public modalRef: MatDialogRef<ChatmodalComponent>,
+  private mensajeService: MensajeService,
+  private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.mensajesEnviados = this.data.msgEnviados as Mensaje[],
-    this.mensajesRecibidos = this.data.msgRecibidos as Mensaje[]
-    console.log("Desde el modal");
-    console.log(this.mensajesEnviados);
-    console.log(this.mensajesRecibidos);
+    this.refrescarMensajes();
   }
 
   cancelar(): void{
@@ -29,7 +30,38 @@ export class ChatmodalComponent implements OnInit {
   }
 
   enviarMensaje() {
+    let usuarioReceptorId = this.mensajesEnviados[0].idReceiverUser;
+    let usuarioEmisorId = this.authService.usuario.sub;
+    const mensaje = new Mensaje();
+    mensaje.idEmitterUser = usuarioEmisorId;
+    mensaje.idReceiverUser = usuarioReceptorId;
+    mensaje.text = this.textoNuevoMensaje;
+    this.mensajeService.crear(mensaje).subscribe(mensaje => {
+      Swal.fire('Mensaje enviado: ',
+        'Mensaje enviado con éxito',
+        'success');
 
+      // refrescar lista
+      this.refrescarMensajes();
+      this.textoNuevoMensaje = "";
+    },
+      estado => {
+        if (estado.status === 500) {
+          const mensaje = estado.error.message as string;
+          if (mensaje.indexOf('ConstraintViolationException') > 1) {
+            Swal.fire(
+              'Cuidado',
+              'No se ha podido enviar el mensaje',
+              'error'
+            );
+          }
+        }
+      });
+  }
+
+  refrescarMensajes(): void {
+    this.mensajesEnviados = this.data.msgEnviados as Mensaje[];
+    this.mensajesRecibidos = this.data.msgRecibidos as Mensaje[];
   }
 
 }
